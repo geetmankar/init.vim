6199151ff511008c23c6dda9111343eefae0ab62
Geet Mankar <27027921+geetmankar@users.noreply.github.com>
2025-02-11 13:10:02 +0100

    add: ssh02, ssh03

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,3 @@
-vim.api.nvim_create_user_command("ssh01", "RemoteSSHFSConnect gmankar@euclid01:/home/gmankar/work/num_density", {})
-vim.api.nvim_create_user_command("ssh02", "RemoteSSHFSConnect gmankar@euclid02:/home/gmankar/work/num_density", {})
-vim.api.nvim_create_user_command("ssh03", "RemoteSSHFSConnect gmankar@euclid03:/home/gmankar/work/num_density", {})
+vim.api.nvim_create_user_command("Ssh01", "RemoteSSHFSConnect gmankar@euclid01:/home/gmankar/work/num_density", {})
+vim.api.nvim_create_user_command("Ssh02", "RemoteSSHFSConnect gmankar@euclid02:/home/gmankar/work/num_density", {})
+vim.api.nvim_create_user_command("Ssh03", "RemoteSSHFSConnect gmankar@euclid03:/home/gmankar/work/num_density", {})
