0a557699de8b3c10a6e740b78d98ba4fef99fa80
Geet Mankar <27027921+geetmankar@users.noreply.github.com>
2025-02-12 14:54:17 +0100

    add: more ssh options

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,11 @@
-vim.api.nvim_create_user_command("Ssh01", "RemoteSSHFSConnect gmankar@euclid01:/home/gmankar/work/num_density", {})
-vim.api.nvim_create_user_command("Ssh02", "RemoteSSHFSConnect gmankar@euclid02:/home/gmankar/work/num_density", {})
-vim.api.nvim_create_user_command("Ssh03", "RemoteSSHFSConnect gmankar@euclid03:/home/gmankar/work/num_density", {})
+vim.api.nvim_create_user_command("Ssh01", "RemoteSSHFSConnect gmankar@euclid01:/home/gmankar/", {})
+vim.api.nvim_create_user_command("Ssh02", "RemoteSSHFSConnect gmankar@euclid02:/home/gmankar/", {})
+vim.api.nvim_create_user_command("Ssh03", "RemoteSSHFSConnect gmankar@euclid03:/home/gmankar/", {})
+
+vim.api.nvim_create_user_command("Num01", "RemoteSSHFSConnect gmankar@euclid01:/home/gmankar/work/num_density", {})
+vim.api.nvim_create_user_command("Num02", "RemoteSSHFSConnect gmankar@euclid02:/home/gmankar/work/num_density", {})
+vim.api.nvim_create_user_command("Num03", "RemoteSSHFSConnect gmankar@euclid03:/home/gmankar/work/num_density", {})
+
+vim.api.nvim_create_user_command("Simdet01", "RemoteSSHFSConnect gmankar@euclid01:/home/gmankar/work/sims_dets", {})
+vim.api.nvim_create_user_command("Simdet02", "RemoteSSHFSConnect gmankar@euclid02:/home/gmankar/work/sims_dets", {})
+vim.api.nvim_create_user_command("Simdet03", "RemoteSSHFSConnect gmankar@euclid03:/home/gmankar/work/sims_dets", {})
